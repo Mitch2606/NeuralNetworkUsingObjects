8ccf5a82f7493b4ca7fe56a14e69dc62c6c989b0
Mitch2606 <63268506+Mitch2606@users.noreply.github.com>
2020-10-15 22:26:12 -0400

    Next step is backprop OO

--- NeuralNetworkUsingObjects/Source.cpp
+++ NeuralNetworkUsingObjects/Source.cpp
@@ -5,16 +5,40 @@
 
 using namespace std;
 
-//Functions
+//Global Variable
+double lr = 0.25;
 
+//Functions
 double randFlt() // used to initialize weight values
 {
 	double randomvalue = (double)rand() / 100000;
 	return randomvalue;
 }
 
-//Structures and objects
+double relu(double x)
+{
+	if (x < 0)
+	{
+		return 0.000000001;
+	}
+	return x;
+}
 
+double derRelu(double x)
+{
+	if (x <= 0)
+	{
+		return 0;
+	}
+	return 1;
+}
+
+double softMax(double x, double total)
+{
+	return (x / total);
+}
+
+//Structures and objects
 template<typename X>
 struct DLnode // Double linked list node for variable arrays
 {
@@ -132,12 +156,18 @@ public:
 		return dataToReturn;
 	}
 
+	DLnode<X>* getTail()
+	{
+		return tail;
+	}
+
 	void EmptyNode()
 	{
 		traversalPtr->data = NULL;
 		traversalPtr->next = NULL;
 		traversalPtr->previous = NULL;
 	}
+
 
 	//DebugFunctions
 	void printList()//prints just the data values
@@ -172,8 +202,25 @@ public:
 			printValues();
 		}
 	}
+
+	int findValue(X searchValue)
+	{
+		traversalPtr = head;
+		for (int i = 0; i < sizeOfList; i++)
+		{
+			if (traversalPtr->data == searchValue)
+			{
+				return i;
+			}
+			traversalPtr = traversalPtr->next;
+		}
+		cout << "OH FUCK" << endl;
+		return -1;
+	}
 };
 
+//NeuralNetwork Things
+//Base unit for the neural network, a nueron
 struct nueron
 {
 	double data;
@@ -184,38 +231,51 @@ struct nueron
 	DLList<nueron*> NextLayerNodes;
 	DLList<nueron*> PreviousLayerNodes;
 };
-
+//The Network itslef, is just a mesh data structure
 class Network
 {
 private:
 	DLList<nueron*> nueronsList;
-	DLList<nueron*> InputNuerons;
-	DLList<nueron*> OutputNuerons;
+	nueron* traversalNueron;
 
 public:
 	int numberOfNodes = 0;
 
+	//Manipulating placement Section
 	Network()
 	{
 		numberOfNodes = 0;
 	}
 	
-	void createNeuron(double data, string name, int place)
+	void createNeuron(string name)
 	{
 		numberOfNodes += 1;
 		nueron* tmp = new nueron;
-
+		tmp->data = NULL;
 		tmp->name = name;
+
 		nueronsList.createNode(tmp);
 
-		if (place == 1)
+	}
+
+	void deleteNeuron(string name)
+	{
+		nueron* nueron1 = findNueronPointer(name);
+		
+		for (int i = 0; i < nueronsList.sizeOfList; i++)
 		{
-			InputNuerons.createNode(tmp);
+			//To Node
+			if (connectionexists(nueronsList.getValue(i)->name, name))
+			{
+				disconnectNeuron(nueronsList.getValue(i)->name, name);
+			}
+			//From Node
+			if (connectionexists(name, nueronsList.getValue(i)->name))
+			{
+				disconnectNeuron(name, nueronsList.getValue(i)->name);
+			}
 		}
-		else if(place == 0)
-		{
-			OutputNuerons.createNode(tmp);
-		}
+		nueronsList.removeNode(nueron1);
 	}
 
 	nueron* findNueronPointer(string searchString)
@@ -242,7 +302,11 @@ public:
 	{
 		nueron* nueron1 = findNueronPointer(Name1);
 		nueron* nueron2 = findNueronPointer(Name2);
-
+		if (connectionexists(Name1, Name2))
+		{
+			cout << "Connection Already Exists! " << endl;
+			return;
+		}
 		nueron1->NextLayerNodes.createNode(nueron2);
 		nueron2->PreviousLayerNodes.createNode(nueron1);
 
@@ -256,11 +320,8 @@ public:
 
 		if (connectionexists(Name1, Name2))
 		{
-			
 			nueron1->NextLayerNodes.removeNode(nueron2);
-			
 			nueron2->PreviousLayerNodes.removeNode(nueron1);
-
 		}
 		return;
 	}
@@ -275,7 +336,7 @@ public:
 		for (int i = 0; i < nueron1->NextLayerNodes.sizeOfList; i++)
 		{
 			//Check if names is in the list
-			if ((nueron1->NextLayerNodes.getValue(i)->name == Name2) || (nueron2->PreviousLayerNodes.getValue(i)->name == Name1))
+			if (nueron1->NextLayerNodes.getValue(i)->name == Name2)
 			{
 				return true;
 			}
@@ -285,25 +346,173 @@ public:
 
 	void showAllConnections()
 	{
-		nueron* nueron1 = nueronsList.getValue(0);
+		traversalNueron = nueronsList.getValue(0);
 
 		for (int i = 0; i < nueronsList.sizeOfList; i++)
 		{
-			nueron1 = nueronsList.getValue(i);
+			traversalNueron = nueronsList.getValue(i);
 
-			cout << "\nNode " << nueron1->name << " is connected to "; 
-			if (nueron1->NextLayerNodes.sizeOfList == 0)
+			cout << "\nNode " << traversalNueron->name << " is connected to "; 
+			if (traversalNueron->NextLayerNodes.sizeOfList == 0)
 			{
 				cout << "nothing.";
 				continue;
 			}
-			nueron1->NextLayerNodes.printList();
+			traversalNueron->NextLayerNodes.printList();
 
 		}
 
 	}
 	
+	void showAllNetworkValues()
+	{
+		traversalNueron = nueronsList.getValue(0);
+		for (int i = 0; i < nueronsList.sizeOfList; i++)
+		{
+			traversalNueron = nueronsList.getValue(i);
+			for (int i = 0; i < traversalNueron->NextLayerNodes.sizeOfList; i++)
+			{
+				cout << traversalNueron->name << ":" << traversalNueron->data << " is connected to ";
+				cout << traversalNueron->NextLayerNodes.getValue(i)->name << ":" << traversalNueron->data;
+				cout << " with a weight of ";
+				cout << traversalNueron->weights.getValue(i) << endl;
+			}
+		} 
+
+	}
+
+	void showAllNodeNames()
+	{
+		traversalNueron = nueronsList.getValue(0);
+		for (int i = 0; i < nueronsList.sizeOfList; i++)
+		{
+			traversalNueron = nueronsList.getValue(i);
+
+			cout << traversalNueron->name << ", ";
+		}
+	}
 	
+	//Actual Math Section
+	void run(int apoches, double inputs[4][2], double PO[4][2])
+	{
+		nueron* temppointer;
+		initializeWeightsOfNodes();
+
+		temppointer = findNueronPointer("EndNode");
+		
+		for (int i = 0; i < (apoches * 4); i++)
+		{
+			cout << endl << "___________________" << endl;
+			setInputs(inputs, PO);
+
+			calculateData(temppointer);
+			getOutputs();
+			//backpropagation(findNueronPointer("fakeNueron"));
+			resetNetwork();
+			cout << "Apoche: " << i;
+		}
+	}
+
+	void setInputs(double inputs[4][2], double PO[4][2])
+	{
+		int i1 = 0;
+		static int picker = 0;
+
+		cout << "Inputs: " << inputs[picker][0] << " , " << inputs[picker][1] << endl;
+		cout << "Prefer: " << inputs[picker][0] << " , " << inputs[picker][1] << endl;
+
+		for (int i = 0; i < nueronsList.sizeOfList; i++)
+		{
+			if (nueronsList.getValue(i)->PreviousLayerNodes.sizeOfList == 0)
+			{
+				nueronsList.getValue(i)->data = inputs[picker][i1];
+				i1++;
+			}
+		}
+		picker++;
+		if (picker > 3)
+		{
+			picker = 0;
+		}
+	}
+
+	void getOutputs()
+	{
+		for (int i = 0; i < nueronsList.sizeOfList; i++)
+		{
+			if (nueronsList.getValue(i)->NextLayerNodes.sizeOfList != 0) {
+				if (nueronsList.getValue(i)->NextLayerNodes.getValue(0)->name == "EndNode")
+				{
+					cout << nueronsList.getValue(i)->data << ", ";
+				}
+			}
+		}
+		cout << endl;
+	}
+
+	void initializeWeightsOfNodes()
+	{
+		nueron* traversalNueron = nueronsList.getValue(0);
+		for (int i = 0; i < nueronsList.sizeOfList; i++)
+		{
+			traversalNueron = nueronsList.getValue(i);
+			for (int i = 0; i < traversalNueron->NextLayerNodes.sizeOfList; i++)
+			{
+				traversalNueron->weights.createNode(randFlt());
+			}
+			
+			//cout << traversalNueron->name << endl;
+		}
+	}
+
+	void calculateData(nueron* passedNueron)
+	{
+		nueron* pNueron;
+
+		int index;
+		double temp;
+		
+		//Calculates the values
+		temp = 0;
+		for (int i = 0; i < passedNueron->PreviousLayerNodes.sizeOfList; i++)
+		{
+			pNueron = passedNueron->PreviousLayerNodes.getValue(i);
+
+			//Checks if the values of the previous node have been calculated yet
+			if (pNueron->data == 0)
+			{
+				calculateData(pNueron);
+			}
+
+			index = pNueron->NextLayerNodes.findValue(passedNueron);
+
+			//cout << index << " " << passedNueron->name << " " << pNueron->name << " ";
+			//cout << pNueron->data << " " << pNueron->weights.getValue(index) << endl;
+			temp += pNueron->data * pNueron->weights.getValue(index);
+		}
+		//cout << " temp: " << temp << endl;
+		passedNueron->data = relu(temp);
+
+	}
+
+	void backpropagation()
+	{
+
+	}
+
+	void resetNetwork()
+	{
+		traversalNueron = nueronsList.getValue(0);
+		for (int i = 0; i <= nueronsList.sizeOfList; i++)
+		{	
+			if (traversalNueron->PreviousLayerNodes.sizeOfList != 0)
+			{
+				traversalNueron->data = 0.0;
+			}
+			traversalNueron = nueronsList.getValue(i);
+		}
+	}
+
 };
 
 
@@ -313,23 +522,60 @@ class GUI
 public:
 
 	Network networkBoi;
+	double inputs[4][2] = { {0,0},{0,1},{1,0},{1,1} };
+	double preffered[4][2] = { {0,0},{0,1},{0,1},{1,1} };
+	int apoches = 1;
 
 	GUI()
 	{
 		cout << "Program Started: " << endl;
+		cout << "default 2,3,2 network initialized: " << endl;
+		
+		networkBoi.createNeuron("i1");
+		networkBoi.createNeuron("i2");
+
+		networkBoi.createNeuron("h1");
+		networkBoi.createNeuron("h2");
+		networkBoi.createNeuron("h3");
+
+		networkBoi.createNeuron("o1");
+		networkBoi.createNeuron("o2");
+
+		networkBoi.createNeuron("EndNode");
+
+		networkBoi.connectNeuron("i1", "h1");
+		networkBoi.connectNeuron("i1", "h2");
+		networkBoi.connectNeuron("i1", "h3");
+
+		networkBoi.connectNeuron("i2", "h1");
+		networkBoi.connectNeuron("i2", "h2");
+		networkBoi.connectNeuron("i2", "h3");
+
+		networkBoi.connectNeuron("h1", "o1");
+		networkBoi.connectNeuron("h1", "o2");
+
+		networkBoi.connectNeuron("h2", "o1");
+		networkBoi.connectNeuron("h2", "o2");
+
+		networkBoi.connectNeuron("h3", "o1");
+		networkBoi.connectNeuron("h3", "o2");
+		
+		networkBoi.connectNeuron("o1", "EndNode");
+		networkBoi.connectNeuron("o2", "EndNode");
 	}
 
 	void FrontPage()
 	{
+
 		int i = 1;
 		while (i != 0)
 		{
-			cout << "Home Page: " << endl;
-			cout << "____________________________" << endl;
+			cout << endl << "____________________________" << endl;
 			cout << "1 - Add Node: " << endl;
 			cout << "2 - Connect Nodes: " << endl;
-			cout << "3 - Display Network: " << endl;
-			cout << "3 - Run Network: " << endl;
+			cout << "3 - Display Connections: " << endl;
+			cout << "4 - Display Network Values: " << endl;
+			cout << "5 - Run Network: " << endl;
 			cout << "0 - End Program: " << endl;
 			cout << "____________________________" << endl;
 
@@ -337,33 +583,55 @@ public:
 			switch (i)
 			{
 			case(0):
+				break;
 			case(1):
 				
 				addSubNodes();
-				cout << "called function";
 				break;
 			case(2):
+				if (networkBoi.numberOfNodes == 0)
+				{
+					cout << "You have an empty network D:";
+					break;
+				}
 				dis_connectNodes();
 				break;
 			case(3):
+				if (networkBoi.numberOfNodes == 0)
+				{
+					cout << "You have an empty network D:";
+					break;
+				}
 				networkBoi.showAllConnections();
+				break;
+			case(4):
+				if (networkBoi.numberOfNodes == 0)
+				{
+					cout << "You have an empty network D:";
+					break;
+				}
+				networkBoi.showAllNetworkValues();
+				break;
+			case(5):
+				if (networkBoi.numberOfNodes == 0)
+				{
+					break;
+				}
+				
+				cin >> apoches;
+
+				networkBoi.run(apoches, inputs, preffered);
 				break;
 			default:
 				break;
-				//case(4):
-				//	network.run();
 			}
 		}
 	}
-
 	void addSubNodes()
 	{
 		int i = 1;
 
-		double data = 0;
 		string name;
-		int place;
-
 
 		while (i != 0)
 		{
@@ -373,6 +641,7 @@ public:
 			cout << "1 - Add Node:" << endl;
 			cout << "2 - Sub Node:" << endl;
 			cout << "0 - return: " << endl;
+			cout << "____________________________" << endl;
 
 			cin >> i;
 
@@ -383,10 +652,15 @@ public:
 			case(1):
 				cout << "Input UNIQUE name for node: " << endl;
 				cin >> name;
-				cout << "Specifive I / O or H: 1,0,2: ";
-				cin >> place;
+				
+				networkBoi.createNeuron(name);
+				break;
+			case(2):
+				cout << "Input name of node: " << endl;
+				cin >> name;
 
-				networkBoi.createNeuron(data, name, place);
+				networkBoi.deleteNeuron(name);
+				break;
 			}
 		}
 	}
@@ -411,7 +685,9 @@ public:
 			case(0):
 				break;
 			case(1):
-				cout << "Input Nueron1's name, then nueron 2's" << endl; 
+				cout << "Input Nueron1's name, then nueron 2's" << endl << "Avaliable node names: ";
+				networkBoi.showAllNodeNames();
+				cout << endl;
 				
 				cin >> name1;
 				cin >> name2;
@@ -419,7 +695,9 @@ public:
 				networkBoi.connectNeuron(name1, name2);
 				break;
 			case(2):
-				cout << "Input Nueron1's name, then nueron 2's" << endl;
+				cout << "Input Nueron1's name, then nueron 2's" << endl << "Avaliable node names: ";
+				networkBoi.showAllNodeNames();
+				cout << endl;
 
 				cin >> name1;
 				cin >> name2;
@@ -430,7 +708,6 @@ public:
 
 		}
 	}
-
 
 };
 
